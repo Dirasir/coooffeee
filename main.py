7620f133243e082455389c53f6628a177d98fd2c
Dirasir <baicyrovdaniil2007@gmail.om>
2022-11-26 16:29:27 +0300

    - проверка

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import sys
-import sqlite3
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5 import uic
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QWidget
-import random
-from PyQt5.QtWidgets import QLabel, QPushButton
-
-
-
+from PyQt5.QtWidgets import QMainWindow
 class Main_Window(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
         db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('coffee.sqlite')
+        db.setDatabaseName('coffe.db')
         db.open()
         model = QSqlTableModel(self, db)
-        model.setTable('coffe')
+        model.setTable('coffeee')
         model.select()
         self.tableView.setModel(model)
         self.tableView.show()
-
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
